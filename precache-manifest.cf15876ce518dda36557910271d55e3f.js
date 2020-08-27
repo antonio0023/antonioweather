1c61b41a9c306367e929eb63ed34742c05c90195
Antonio Chavez <antonio.2310cv@gmail.com>
2020-08-27 01:07:39 -0600

    Updates

---
+++ precache-manifest.cf15876ce518dda36557910271d55e3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2826066d569628b0120d8caa5c9e44d1",
+    "url": "/antonioweather/index.html"
+  },
+  {
+    "revision": "2f1ede03ef0088ac0269",
+    "url": "/antonioweather/static/css/main.b2a1a2ea.chunk.css"
+  },
+  {
+    "revision": "f96f5c7409679b594464",
+    "url": "/antonioweather/static/js/2.840fdd64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/antonioweather/static/js/2.840fdd64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1ede03ef0088ac0269",
+    "url": "/antonioweather/static/js/main.ced60a23.chunk.js"
+  },
+  {
+    "revision": "04c19c13c185659a3487",
+    "url": "/antonioweather/static/js/runtime-main.842ac6fa.js"
+  },
+  {
+    "revision": "402f492f200e4ee428dbf3fe90bd3022",
+    "url": "/antonioweather/static/media/dayweather.402f492f.png"
+  },
+  {
+    "revision": "ba87b795e266147aeb611b5bd6387402",
+    "url": "/antonioweather/static/media/nightweather.ba87b795.png"
+  }
+]);
